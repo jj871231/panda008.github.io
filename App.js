e566dae7448b3cda658d365df27a4c4925f2fa87
liangjj <jj871231@126.com>
2014-01-13 15:16:35 +0800

    jj871231

---
+++ App.js
@@ -0,0 +1,129 @@
+/*!
+ * Ext JS Library 4.0
+ * Copyright(c) 2006-2011 Sencha Inc.
+ * licensing@sencha.com
+ * http://www.sencha.com/license
+ */
+
+Ext.define('MyDesktop.App', {
+    extend: 'Ext.ux.desktop.App',
+
+    requires: [
+        'Ext.window.MessageBox',
+
+        'Ext.ux.desktop.ShortcutModel',
+
+        'Desk.modules.SystemStatus',
+        'Desk.modules.VideoWindow',
+        'Desk.modules.GridWindow',
+        'Desk.modules.TabWindow',
+        'Desk.modules.AccordionWindow',
+        'Desk.modules.Notepad',
+        'Desk.modules.BogusMenuModule',
+        'Desk.modules.BogusModule',
+
+//        'MyDesktop.Blockalanche',
+        'Desk.modules.Settings'
+    ],
+
+    init: function() {
+        // custom logic before getXYZ methods get called...
+
+        this.callParent();
+
+        // now ready...
+    },
+
+    getModules : function(){
+        return [
+            new Desk.modules.VideoWindow(),
+            //new MyDesktop.Blockalanche(),
+            new Desk.modules.SystemStatus(),
+            new Desk.modules.GridWindow(),
+            new Desk.modules.TabWindow(),
+            new Desk.modules.AccordionWindow(),
+            new Desk.modules.Notepad(),
+            new Desk.modules.BogusMenuModule(),
+            new Desk.modules.BogusModule()
+        ];
+    },
+
+    getDesktopConfig: function () {
+        var me = this, ret = me.callParent();
+
+        return Ext.apply(ret, {
+            //cls: 'ux-desktop-black',
+
+            contextMenuItems: [
+                { text: 'Change Settings', handler: me.onSettings, scope: me }
+            ],
+
+            shortcuts: Ext.create('Ext.data.Store', {
+                model: 'Ext.ux.desktop.ShortcutModel',
+                data: [
+                    { name: 'Grid Window', iconCls: 'grid-shortcut', module: 'grid-win' },
+                    { name: 'Accordion Window', iconCls: 'accordion-shortcut', module: 'acc-win' },
+                    { name: 'Notepad', iconCls: 'notepad-shortcut', module: 'notepad' },
+                    { name: 'System Status', iconCls: 'cpu-shortcut', module: 'systemstatus'}
+                ]
+            }),
+
+            wallpaper: 'resources/wallpapers/Blue-Sencha.jpg',
+            wallpaperStretch: false
+        });
+    },
+
+    // config for the start menu
+    getStartConfig : function() {
+        var me = this, ret = me.callParent();
+
+        return Ext.apply(ret, {
+            title: 'Don Griffin',
+            iconCls: 'user',
+            height: 300,
+            toolConfig: {
+                width: 100,
+                items: [
+                    {
+                        text:'Settings',
+                        iconCls:'settings',
+                        handler: me.onSettings,
+                        scope: me
+                    },
+                    '-',
+                    {
+                        text:'Logout',
+                        iconCls:'logout',
+                        handler: me.onLogout,
+                        scope: me
+                    }
+                ]
+            }
+        });
+    },
+
+    getTaskbarConfig: function () {
+        var ret = this.callParent();
+
+        return Ext.apply(ret, {
+            quickStart: [
+                { name: 'Accordion Window', iconCls: 'accordion', module: 'acc-win' },
+                { name: 'Grid Window', iconCls: 'icon-grid', module: 'grid-win' }
+            ],
+            trayItems: [
+                { xtype: 'trayclock', flex: 1 }
+            ]
+        });
+    },
+
+    onLogout: function () {
+        Ext.Msg.confirm('Logout', 'Are you sure you want to logout?');
+    },
+
+    onSettings: function () {
+        var dlg = new Desk.modules.Settings({
+            desktop: this.desktop
+        });
+        dlg.show();
+    }
+});
